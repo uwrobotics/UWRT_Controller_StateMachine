a4c17f53b91cd56a5afdb370f4bcf0f7f78cb967
billylin609 <linyuchen830@gmail.com>
2025-02-11 12:48:04 -0500

    f

---
+++ controller_statemachine/src/publish_vel.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "controller_statemachine/msg/CmdVel.hpp"
+
+using namespace std::chrono_literals;
+
+class CmdVelPublisher : public rclcpp::Node
+{
+public:
+  CmdVelPublisher()
+  : Node("cmd_vel_publisher")
+  {
+    address_book_publisher_ =
+      this->create_publisher<controller_statemachine::msg::CmdVel>("cmd_vel", 10);
+
+    auto publish_msg = [this]() -> void {
+        auto message = more_interfaces::msg::CmdVel();
+
+        message.axis_id = 1;
+        message.cmd = "Vel";
+        message.speed = 10;
+
+        this->cmd_vel_publisher->publish(message);
+      };
+    timer_ = this->create_wall_timer(1s, publish_msg);
+  }
+
+private:
+  rclcpp::Publisher<more_interfaces::msg::AddressBook>::SharedPtr cmd_vel_publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CmdVelPublisher>());
+  rclcpp::shutdown();
+
+  return 0;
+}
