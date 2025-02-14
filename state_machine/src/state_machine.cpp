bb98a0e436f149c6c0a1584d9c4940072b7fd0cc
billylin609 <linyuchen830@gmail.com>
2025-02-14 12:04:49 -0500

    f

--- state_machine/src/state_machine.cpp
+++ state_machine/src/state_machine.cpp
@@ -33,9 +33,7 @@ bool StateMachine::request_odrive_cmd(const std::string &axis_id, const std::str
     }
 
     auto result = motor_cmd_->async_send_request(request);
-    result.wait_for(2s);
-
-    std::cout << result << std::endl;
+    auto future = result.wait_for(2s);
 
     // if (result.valid() && result.wait_for(2s) == std::future_status::ready) {
     //     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Received response: %d", result.get()->status);
