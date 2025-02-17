40ed9c269abecc069b977d97ede18d955fa9eb94
billylin609 <linyuchen830@gmail.com>
2025-02-17 12:29:07 -0500

    f

--- state_machine/include/state_machine.hpp
+++ state_machine/include/state_machine.hpp
@@ -22,7 +22,7 @@
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 
 /* Custom Message */
-#include "nlohmann/json.hpp"
+#include "json_transport/json_transport.hpp"
 #include "uwrt_ros_msg/msg/odrive_cmd.hpp"
 #include "uwrt_ros_msg/msg/msg_response.hpp"
 
